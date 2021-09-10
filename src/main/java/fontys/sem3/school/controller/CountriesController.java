c411a29936ee5f2b7ed51aadfab48474923ceafb
Angel Chernaev <chernaevangel@gmail.com>
2021-09-10 12:21:37 +0200

    pishka

--- src/main/java/fontys/sem3/school/controller/CountriesController.java
+++ src/main/java/fontys/sem3/school/controller/CountriesController.java
@@ -1,10 +1,7 @@
 package fontys.sem3.school.controller;
 
 import fontys.sem3.school.repository.FakeDataStore;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/countries")
@@ -13,7 +10,15 @@ public class CountriesController {
 
     private static final FakeDataStore fakeDataStore=new FakeDataStore();
 
-
+    @GetMapping("/hello")
+    // return only the body, and let Spring boot handle http statuscode
+    // and header
+    @ResponseBody
+    public String SayHello()
+    {
+        String msg = "Hello, your resources work!";
+        return msg;
+    }
 
 
 
